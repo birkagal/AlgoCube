d83e23cc969cee71111180b6ae04cbfe8c406ac7
Gal Birka <birkagal@gmail.com>
2020-08-03 20:18:42 +0300

    Adding base activities

---
+++ app/src/main/java/com/example/algocube/Activity_Timer.java
@@ -0,0 +1,14 @@
+package com.example.algocube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__timer);
+    }
+}
